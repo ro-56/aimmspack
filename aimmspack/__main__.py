b841292b14fe3a51a3d6c4bb82adbb07f21126d4
Rodrigo <31988896+ro-56@users.noreply.github.com>
2021-07-27 19:26:48 -0300

    feat: adds 'show example configuration' cli argument

--- aimmspack/__main__.py
+++ aimmspack/__main__.py
@@ -1,27 +1,32 @@
 from aimmspack.api import debugCLI, aimmspackCLI, example
 from argparse import ArgumentParser
-import aimmspack
 
 def main():    
     parser = ArgumentParser()
-    parser.add_argument('filepath', help='The filepath of the .yaml configuration file')
-    parser.add_argument('-ex', '--example', help='Returns an example of the .yaml configuration file')
-    # parser.add_argument('-v', '--verbose', action='count', default=0, help='foo help')
-    parser.add_argument('-d', '--debug', action='count', default=0, help='debug')
+    subparsers = parser.add_subparsers(dest='mode')
+    
+    parser_build = subparsers.add_parser('build', help='Builds the application defined in a configuration file')
+    parser_build.add_argument('filepath', help='The filepath of the configuration file')
+    parser_build.add_argument('-d', '--debug', action='store_true')
+    group_opt = parser_build.add_mutually_exclusive_group()
+    group_opt.add_argument('-v', '--verbose', action='store_true')
+    group_opt.add_argument('-q', '--quiet', action='store_true')
+
+    parser_example = subparsers.add_parser('example', help='Returns an example of the .yaml configuration file')
 
     args = parser.parse_args()
 
-    if args.example:
+
+    if args.mode == 'example':
         example.print_example_file()
         return
 
-    if args.filepath:
-        if args.debug:
-            debugCLI.test(args.filepath)
-            return
+    if args.mode == 'build':
+        if args.filepath:
+            if args.debug:
+                debugCLI.test(args.filepath)
+                return
 
-        aimmspackCLI.aimmspack(args.filepath)
-    else:
-        print('ERROR')
-
-
+            aimmspackCLI.aimmspack(args.filepath)
+    
+    print('ERROR')
